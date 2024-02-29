981e69deef4decefdece27751e29f65c9de598d2
Sergiu Chiriac <>
2024-02-29 16:57:43 +0000

    update db and readme

--- directories/u.ts
+++ directories/u.ts
@@ -2,15 +2,6 @@ import {Directory} from "../types/directory";
 
 export const directories: Directory[] = [
     {
-        "name": "Uneed Best",
-        "link": "https://www.uneed.best/",
-        "submission_link": "https://www.uneed.best/submit-a-tool",
-        "domain_rating": 26,
-        "monthly_visits": 252000,
-        "submission_experience": "Good",
-        "pricing": "Paid"
-    },
-    {
         "name": "Under1000MRR.tools",
         "link": "https://under1000mrr.tools/",
         "submission_link": "https://under1000mrr.tools/submit-product",
